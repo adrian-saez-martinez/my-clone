3e811599d5ea65cb0462852a7a2698572b5de42e
asaezctcon <asaezctcon@gmail.com>
2024-12-16 17:18:49 +0100

    contact page

--- app.py
+++ app.py
@@ -3,12 +3,12 @@ import streamlit as st
 st.set_page_config(page_title="Adrian Saez Martinez", layout="wide")
 
 resume = st.Page("pages/1_resume.py", title="Resume", icon=":material/home:")
-personal_info = st.Page("pages/2_contact.py", title="Contact", icon=":material/info:")
+conctact_info = st.Page("pages/2_contact.py", title="Contact", icon=":material/info:")
 chatbot = st.Page("pages/3_chatbot.py", title="Ask me anything about Adrián", icon=":material/smart_toy:")
 
 pg = st.navigation(
     {
-        "About me": [resume, personal_info],
+        "About me": [resume, conctact_info],
         "AI Assistant": [chatbot],
     }
 )
