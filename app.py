d0b4d76dca5eab93386b7439749b8f2df90db0e6
adrian-saez-martinez <adriansaezmartinez@gmail.com>
2024-12-20 09:01:54 +0100

    retierver working

--- app.py
+++ app.py
@@ -5,11 +5,12 @@ st.set_page_config(page_title="Adrian Saez Martinez", layout="wide")
 resume = st.Page("pages/1_resume.py", title="Resume", icon=":material/home:")
 conctact_info = st.Page("pages/2_contact.py", title="Contact", icon=":material/info:")
 chatbot = st.Page("pages/3_chatbot.py", title="Ask me anything about Adrián", icon=":material/smart_toy:")
+chatbot_test = st.Page("pages/4_chatbot_test.py", title="TEST_Ask me anything about Adrián", icon=":material/smart_toy:")
 
 pg = st.navigation(
     {
         "About me": [resume, conctact_info],
-        "AI Assistant": [chatbot],
+        "AI Assistant": [chatbot,chatbot_test],
     }
 )
 
